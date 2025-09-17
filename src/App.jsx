f7bb313a9d5dd6186cf59871f11bbed221824877
bambo01 <157558736+bambo01@users.noreply.github.com>
2025-09-17 20:07:54 +0800

    image updates

--- src/App.jsx
+++ src/App.jsx
@@ -3,39 +3,60 @@ import Input from './Input'
 
 function App() {
   return (
-    <div 
-      className="w-full min-h-screen bg-cover bg-center" 
+    <div
+      className="w-full min-h-screen bg-cover bg-center"
       style={{ backgroundImage: "url('/bg.png')" }}
     >
-      {/* Wrapper: flex-col on mobile, row on md+ */}
-      <div className="flex flex-col md:flex-row justify-between items-center w-full px-6 md:px-20 py-10 md:py-0 min-h-screen">
+      {/* Wrapper: flex-col on small, row on md+ */}
+      <div className="flex flex-col md:flex-row justify-between items-center w-full px-6 md:px-12 lg:px-20 py-10 min-h-screen gap-10">
         
-        {/* LEFT: Centered text block */}
-        <div className="flex flex-col justify-center items-center flex-1 text-center md:text-left space-y-6 xl:-mr-70">
-          <div className='flex flex-col items-center'>
-            <h1 className="text-5xl md:text-8xl text-white font-bebas">Maelyn's</h1>
-            <h1 className="text-4xl md:text-7xl text-white font-bebas -mt-2 md:-mt-4">Birthday</h1>
+        {/* LEFT: Text + RSVP */}
+        <div className="flex flex-col justify-center items-center md:items-start flex-1 text-center md:text-left space-y-6">
+          {/* Title */}
+          <div className="flex flex-col items-center md:items-start">
+            <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl text-white font-bebas">
+              Maelyn's
+            </h1>
+            <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl text-white font-bebas -mt-2 md:-mt-4">
+              Birthday
+            </h1>
           </div>
 
+          {/* Invitation details */}
           <div className="space-y-4">
-            <h1 className="text-lg md:text-2xl font-semibold text-white">
+            <h1 className="text-base sm:text-lg md:text-xl lg:text-2xl font-semibold text-white">
               You're invited to my birthday party!
             </h1>
-            <div className='flex flex-col items-center justify-center'>
-             <div className='flex flex-col items-center font-semibold mb-2 text-xl text-white'>
-               <h1>September 23, 2025</h1>
-               <h1>5:00 PM</h1>
-             </div>
-               <img src="/inasal.png" alt="Birthday Pic 2" className="w-40 md:w-40 rounded-lg" />
+
+            <div className="flex flex-col items-center md:items-start justify-center">
+              <div className="flex flex-col items-center md:items-start font-semibold mb-2 text-lg sm:text-xl text-white">
+                <h1>September 23, 2025</h1>
+                <h1>5:00 PM</h1>
+              </div>
+              <img
+                src="/inasal.png"
+                alt="Birthday Pic 2"
+                className="w-32 sm:w-36 md:w-40 rounded-lg"
+              />
             </div>
+
+            {/* RSVP Form */}
             <Input />
           </div>
         </div>
 
         {/* RIGHT: Image block */}
-        <div className="flex gap-4 md:space-y-4 mt-8 md:mt-0 md:flex-col">
-          <img src="/pic1.jpg" alt="Birthday Pic 1" className="w-60 md:w-80 rounded-lg" />
-          <img src="/pic1.jpg" alt="Birthday Pic 2" className="w-60 md:w-80 rounded-lg" />
+        <div className="flex flex-row md:flex-col gap-4">
+          <img
+            src="/pic1.jpg"
+            alt="Birthday Pic 1"
+            className="w-36 sm:w-48 md:w-60 lg:w-72 xl:w-80 rounded-lg"
+          />
+          <img
+            src="/pic1.jpg"
+            alt="Birthday Pic 2"
+            className="w-36 sm:w-48 md:w-60 lg:w-72 xl:w-80 rounded-lg"
+          />
         </div>
       </div>
     </div>
